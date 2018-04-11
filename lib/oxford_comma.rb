fd1c9903dd8941769bb4d1d265daa980447ee1cf
Richard Cho <richardcho100@gmail.com>
2018-04-11 20:07:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    last_one = array.pop
+    list = array.join(", ")
+    list << ", and "
+    list + last_one
+  end
 end
